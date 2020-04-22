f985124ee8be885de32725ca0ce189c7a981e3b5
Amit NB <amit@lollypop.design>
2020-04-22 05:30:26 +0530

    homepage started

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-console.log("Hi, my name is Ukyo!"); // eslint-disable-line no-console
+import $ from 'jquery';
+import 'slick-carousel';
+
+console.log("Hi, this is Ainqa!"); // eslint-disable-line no-console
+
+
+$('.js-homepage-review').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    appendArrows: '.slider-arrows',
+    appendDots: '.slider-dots'
+  });
